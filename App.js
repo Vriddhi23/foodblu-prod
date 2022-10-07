5726c04cca3d2f1ec34f917f78ebf10e23a63574
vriddhi <vriddhi1823@gmail.com>
2022-10-07 21:46:24 +0530

    updated files

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';  
+import React, {useState}  from 'react';  
 import {StyleSheet, Text, View,SafeAreaView,StatusBar,ScrollView,TextInput,Image,props,Button} from 'react-native';  
 import { NavigationContainer } from '@react-navigation/native';
 import welcome from './components/welcome';
@@ -7,13 +7,24 @@ import Flatbutton from './components/loginbutton';
 import Flatbuttonn from './components/requestotp';
 import Flatbuttoon from './components/signupbutton';
 import loginScreen from './components/loginpage';
+import mainscreen from './components/mainpage';
 
 function Signupscreen(props){
+  const [textInputName, setTextInputName] = useState('');
 
   const NavigateToLogin = () =>{
-    props.navigation.navigate('login');
-  } 
+    
+      //Check for the Name TextInput
+      if (!textInputName.trim()) {
+        alert('Please Fill up all the values.');
+        return;
+      }
+      //Checked Successfully
+      //Do whatever you want
+      props.navigation.navigate('login');
+    };
   
+
   return (
     <SafeAreaView>
      <ScrollView>
@@ -31,16 +42,24 @@ function Signupscreen(props){
        placeholder ="EMPLOYEE ID"
        autoCapitalize='none'
        autoCorrect={false}
+       onChangeText={
+        (value) => setTextInputName(value)
+      }
       
        />
         <TextInput
        style={styles.input}
        placeholder ="USER NAME"
+       onChangeText={
+        (value) => setTextInputName(value)
+      }
        />
        <TextInput
        style={styles.input}
        placeholder ="PHONE NUMBER"
-         
+       onChangeText={
+        (value) => setTextInputName(value)
+      }
        />
        <TextInput
        style={styles.input}
@@ -50,6 +69,9 @@ function Signupscreen(props){
        iconPosition="right"
        autoCapitalize='none'
        autoCorrect={false}
+       onChangeText={
+        (value) => setTextInputName(value)
+      }
        />
      
        <Flatbuttoon text='Sign Up' onPress={()=>NavigateToLogin()}/>
@@ -64,146 +86,6 @@ function Signupscreen(props){
 };
 
 
-/*function loginScreen() {
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={{backgroundColor:'#ffffff'}}>
-      <View style={styles.view1}>
-      <Image source= { require('./components/loginimage.png')} style={{width:250,height:270,marginTop:-20}} />
-      </View>
-      <View style={styles.view2}>
-      <Text style={styles.text1}>Login</Text>
-      </View>
-      <View style={styles.view3}>
-      <TextInput style={styles.input1} placeholder="Enter Employee Id" autoCapitalize='none' autoCorrect={false}/>
-      <TextInput style={styles.input1} placeholder="Password" 
-      autoCapitalize='none' autoCorrect={false} secureTextEntry={true}
-      />
-      
-      <TextInput style={styles.input1} placeholder="Enter OTP"/>  
-      </View>
-      <View style={styles.button1} >
-        <Flatbuttonn text='Request OTP' onPress={()=>alert("OTP send to your number")}/>
-      
-      </View>
-      <View style={styles.button2}>
-        <Flatbutton text='LOGIN' onPress={()=>alert("Welcome")}/>
-      </View>
-      </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-
-  first:{
-    marginTop:50,
-  },
-    view1:{
-      alignItems:'center',
-      backgroundColor: '#ffffff',
-    },
-    text1:{
-      margin:5,
-      fontWeight:'bold',
-      fontSize: 38,
-      color:'#1E1E1E',
-    },
-    view2:{
-      marginTop: -35,
-      alignItems: 'center',
-      backgroundColor:'#ffffff',
-    },
-    view3:{
-      alignItems:'baseline',
-      marginLeft:-5,
-      borderRadius:10,
-      backgroundColor:'#ffffff',
-    },
-    input1:{
-      backgroundColor:'#ffffff',
-      marginTop:20,
-      marginLeft:25,
-      marginRight:25,
-      width:290,
-      alignSelf:'center',
-      height : 40,  
-      borderWidth: 3,
-      fontWeight: 'bold',
-      borderBottomColor: '#391593',
-      borderTopWidth: 0,
-      borderTopLeftRadius: 0,
-      borderTopRightRadius: 0,
-      borderLeftColor: '#ffffff',
-      borderRightColor: '#ffffff',
-    },
-    button1:{
-      marginTop:10,
-      width:200,
-      marginLeft:206,
-    },
-    button2:{
-      height:250,
-      marginTop:80,
-      alignItems:'center',
-      
-    },
-    text2:{
-      marginTop: 10,
-    }, header:{
-      fontSize: 40,
-      color: '#1E1E1E', 
-      fontWeight: 'bold',
-     },
-     acc:{
-      fontWeight:'bold',
-      fontSize: 24,
-      marginLeft: 20,
-      color:'#1E1E1E',
-      marginBottom:2,
-     },
-     body:{
-      backgroundColor:'#ffffff',
-      marginLeft:-25,
-     },
-     input:{
-      height : 40,
-      width: 290,
-      margin : 18,
-      borderWidth: 3,
-      marginLeft: 40,
-      fontWeight: 'bold',
-      borderBottomColor: '#391593',
-      borderTopWidth: 0,
-      borderTopLeftRadius: 0,
-      borderTopRightRadius: 0,
-      borderLeftColor: '#ffffff',
-      borderRightColor: '#ffffff',
-     },
-     image1:{
-      backgroundColor:'#ffffff', 
-      width: 150,
-      marginLeft:160,
-      height:200,
-    
-    },
-    MainContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor:'#ffffff',
-    },
-    TextStyle: {
-    
-      color: '#E91E63',
-      textDecorationLine: 'underline'
-    
-    }
-  
-});
-*/
 const styles = StyleSheet.create({
 text2:{
   marginTop: 10,
@@ -267,6 +149,7 @@ export default function App(){
         <Stack.Screen name="welcome" component={welcome} />
         <Stack.Screen name="login" component={loginScreen} />
         <Stack.Screen name="signup" component={Signupscreen} />
+       <Stack.Screen name="mainscreen" component={mainscreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
